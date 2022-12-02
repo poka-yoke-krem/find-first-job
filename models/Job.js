05d50982ff024e8c002d51097582983a30188cbf
Ruslan <kremcheev.ruslan@gmail.com>
2022-12-02 05:30:44 +0300

    add server to main folder

---
+++ models/Job.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JobsSchema = mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, "Please provide company name"],
+      minlength: 1,
+      maxlength: 32,
+    },
+    position: {
+      type: String,
+      required: [true, "Please provide position"],
+      minlength: 1,
+      maxlength: 50,
+    },
+    status: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide user"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Job", JobsSchema);
